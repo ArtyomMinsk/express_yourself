97db2011832774d9518968702834d8c9126692d2
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-08-18 01:46:03 -0400

    Deleted comments

--- textminer/validator.py
+++ textminer/validator.py
@@ -30,30 +30,7 @@ def phone_number(example):
 
 
 def money(example):
-    # return re.match(r'^[$]\d+[\.,]?(\d+)?[\.,]?(\d+)?[\.,]?(\d+)?', example)
     return re.match(r'^[$]\d+((\,\d{3})?)*(\.\d{2})?$', example)
-    # (\.\d{2})?
-    # ((\,\d{3})?)*
-# assert v.money("$4")
-# assert v.money("$19")
-# assert v.money("$19.00")
-# assert v.money("$3.58")
-# assert v.money("$1000")
-# assert v.money("$1000.00")
-# assert v.money("$1,000")
-# assert v.money("$1,000.00")
-# assert v.money("$5,555,555")
-# assert v.money("$5,555,555.55")
-# assert v.money("$45,555,555.55")
-# assert v.money("$456,555,555.55")
-# assert v.money("$1234567.89")
-# assert not v.money("")
-# assert not v.money("$12,34")
-# assert not v.money("$1234.9")
-# assert not v.money("$1234.999")
-# assert not v.money("$")
-# assert not v.money("31")
-# assert not v.money("$$31")
 
 
 def zipcode(example):
